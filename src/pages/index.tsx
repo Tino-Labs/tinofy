b96fee0db38ae2535eb8415c54fb50ae545f14e9
Robles AR <tinolabs@yandex.com>
2021-01-08 22:36:37 -0300

    improve home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,15 @@ export default function Home() {
     <>
       <Title>Home</Title>
       <Container className="h-screen flex flex-col items-center justify-center text-center">
-        <div className="max-w-5xl">
-          <h1 className="text-white text-4xl md:text-6xl font-display font-extrabold">
-            If you are good at something never do it for free
+        <div className="max-w-3xl">
+          <h1 className="text-white text-4xl font-display font-medium md:text-5xl md:font-bold">
+            Find all the events and places you are looking.
           </h1>
-          <p className="text-gray-300 text-xl md:text-2xl leading-9 font-medium mt-4 md:mt-8">
-            What is the most resilient parasite? Bacteria? A virus? An intestinal worm? An idea.
-            <span className="font-bold"> Resilient</span>... highly contagious. Once an idea has
-            taken hold of the brain it's almost impossible to eradicate. An idea that is fully
-            formed--fully understood--that sticks.
+          <p className="text-gray-300 text-lg font-normal md:text-2xl leading-9 mt-4 md:mt-8">
+            <span className="underline font-bold">tinofy</span> helps you find all the places to
+            visit and upcoming events with all the information you need.
           </p>
-          <button className="bg-blue-300 font-bold px-4 py-2 rounded-lg mt-4 md:mt-8">
+          <button className="bg-blue-300 md:text-lg font-bold px-4 py-2 md:px-6 md:py-3 rounded-lg mt-4 md:mt-8">
             Let's Get Started
           </button>
         </div>
